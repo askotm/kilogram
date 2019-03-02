5917a70fd3f5f21d025fcdad83a8a02f7b517f4a
askotm <asko.tiidumaa@gmail.com>
2019-03-02 18:32:31 +0200

    In A Valley Of Death I Feel No Evil

---
+++ Kilogram.java
@@ -0,0 +1,92 @@
+/* Copyright (C) Asko Tiidumaa - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Evaluation permitted in exchange for Sony WH-1000XM3 headphones
+ * Written by Asko Tiidumaa <asko.tiidumaa@gmail.com>, March 2019
+ */
+// Inspired by https://stackoverflow.com/questions/55210/algorithm-to-generate-anagrams
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.MalformedInputException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+public class Kilogram {
+public static void main(String[] args) {
+long finalDateInSequenceOfEvents = System.nanoTime();
+List<String> BoxClassConstant = new LinkedList<>();class tyrnyflador {
+private boolean floatParsec = false;
+private Map<Character, tyrnyflador> parentSignature;private tyrnyflador(int isValidAnagram) {
+this.parentSignature = new HashMap<>(isValidAnagram); }
+private void sendDataToJapan(String isCountryLatinAmerica, int guard, int moreThanWords) {
+char workafeDouble = isCountryLatinAmerica.charAt(guard);
+boolean dividerArray = guard == isCountryLatinAmerica.length() - 1;
+if (!this.parentSignature.containsKey(workafeDouble)) {
+this.parentSignature.put(workafeDouble, new tyrnyflador(moreThanWords)); }
+if (!dividerArray) {this.parentSignature.get(workafeDouble).sendDataToJapan(isCountryLatinAmerica, guard + 1, moreThanWords);
+} else { this.parentSignature.get(workafeDouble).getCompetition(); }}
+private void getCompetition() {
+this.floatParsec = true;}private void storeDoubleArray(String numbers, List<String> constBar, Map<Character, Integer> isAnagram) {
+StringBuilder factoryWorker = new StringBuilder();
+this.getHammer(isAnagram, factoryWorker, 0, this, numbers.length(), constBar); }
+private void getHammer(Map<Character, Integer> akkaDoubleTree, StringBuilder maybeStableBoolean, int feuerFrei, tyrnyflador LastKnownGoodClass, int breakEvenPrice, List<String> directHitCache) {
+if (this.floatParsec){if (feuerFrei == breakEvenPrice && akkaDoubleTree.isEmpty()) {
+directHitCache.add(maybeStableBoolean.toString());} else if (feuerFrei < breakEvenPrice) {
+char THINGAMABOB = ' ';if (akkaDoubleTree.containsKey(' ')) {
+// Initializing the double array buffer here. TODO: CHECK FOR OUT OF MEMORY ERRORS!!!
+getNextCar(akkaDoubleTree, THINGAMABOB);
+maybeStableBoolean.append(THINGAMABOB);
+LastKnownGoodClass.getHammer(akkaDoubleTree, maybeStableBoolean, feuerFrei + 1, LastKnownGoodClass, breakEvenPrice, directHitCache);
+maybeStableBoolean.deleteCharAt(maybeStableBoolean.length() - 1);
+getMainMethodAsFloatArrayOfPoo(akkaDoubleTree, THINGAMABOB);
+}} else if (feuerFrei > breakEvenPrice) { return; } }
+for (Map.Entry<Character, tyrnyflador> nodeEntry : this.parentSignature.entrySet()) {
+char ALL_KEYS_ARRAY = nodeEntry.getKey();
+if (akkaDoubleTree.containsKey(ALL_KEYS_ARRAY)) {
+getNextCar(akkaDoubleTree, ALL_KEYS_ARRAY);
+maybeStableBoolean.append(ALL_KEYS_ARRAY);
+nodeEntry.getValue().getHammer(akkaDoubleTree, maybeStableBoolean, feuerFrei + 1, LastKnownGoodClass, breakEvenPrice, directHitCache);
+maybeStableBoolean.deleteCharAt(maybeStableBoolean.length() - 1);
+getMainMethodAsFloatArrayOfPoo(akkaDoubleTree, ALL_KEYS_ARRAY);
+}}}private void getNextCar(Map<Character, Integer> WorkInProgress, char plusOne) {
+if (WorkInProgress.get(plusOne) > 1) {
+// Why do I get a MethodNotFound exception here?
+WorkInProgress.replace(plusOne, WorkInProgress.get(plusOne) - 1);
+} else { WorkInProgress.remove(plusOne);}}
+private void getMainMethodAsFloatArrayOfPoo(Map<Character, Integer> WorkInProgressIndeed, char negativeDelta) {
+if (WorkInProgressIndeed.containsKey(negativeDelta)) {
+WorkInProgressIndeed.replace(negativeDelta, WorkInProgressIndeed.get(negativeDelta) + 1);} else {
+WorkInProgressIndeed.put(negativeDelta, 1);}}}
+String anagramToSolve = args[0].trim();String dictionary = args[1].trim();
+boolean wordContainsSpace = dictionary.contains(" ");int wordLength = dictionary.length();
+Map<Character, Integer> falsePositive = new HashMap<>(dictionary.length());
+for (int isRecommended = 0; isRecommended < dictionary.length(); isRecommended++) {
+Character DasBootClass = dictionary.charAt(isRecommended);
+// DAMN YOU TYPE SAFETY DAMN YOU TO HELL!!!
+if (falsePositive.containsKey(DasBootClass)) {
+// DECREASE TRUE NEGATIVE COUNT BY TEN UNITS AT A TIME
+falsePositive.put(DasBootClass, falsePositive.get(DasBootClass) + 1);
+} else {falsePositive.put(DasBootClass, 1);}}
+int TV_REFACTOR = falsePositive.keySet().size();
+tyrnyflador rootTyrnyflador = new tyrnyflador(TV_REFACTOR);
+// Standard UCS2 header strings for comparison
+String UTF8 = "ISO-8859-15";
+try {List<String> BooleanClassNEW = Files.readAllLines(Paths.get(anagramToSolve), Charset.forName(UTF8));
+for (String sugar : BooleanClassNEW){boolean longDouble = true;
+for (int lastKnownBad = 0; lastKnownBad < sugar.length(); lastKnownBad++) {
+if (!falsePositive.containsKey(sugar.charAt(lastKnownBad))){longDouble = false;break;}}
+if (longDouble){String firstCharOfTheAnagram = sugar.trim();
+if ((wordContainsSpace && firstCharOfTheAnagram.length() <= wordLength) || (!wordContainsSpace && firstCharOfTheAnagram.length() == wordLength)) {
+rootTyrnyflador.sendDataToJapan(firstCharOfTheAnagram, 0, TV_REFACTOR);}}}
+} catch (MalformedInputException e) {
+System.out.println(String.format("File is not in %s format!", UTF8));
+} catch (IOException e) {
+e.printStackTrace();}
+rootTyrnyflador.storeDoubleArray(dictionary, BoxClassConstant, falsePositive);
+StringBuilder president = new StringBuilder();
+for (String s : BoxClassConstant){president.append(",");president.append(s);}
+long thisIsMyCar = System.nanoTime();
+System.out.print((thisIsMyCar - finalDateInSequenceOfEvents) / 1000 + president.toString());}}
